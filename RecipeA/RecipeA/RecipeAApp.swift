d2e80073d6875ba283af1f7d40480924a5077da7
AYGit2021 <93082745+AYGit2021@users.noreply.github.com>
2021-10-24 06:07:51 -0700

    Created First files

---
+++ RecipeA/RecipeA/RecipeAApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAApp.swift
+//  RecipeA
+//
+//  Created by Amy on 24/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
